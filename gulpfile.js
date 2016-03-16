e76b3079c692b47383ba2d90c9e0b87ab5e31c95
Lukasz Kuklis <lkuklis@gmail.com>
2016-03-16 23:52:48 +0100

    added bootstrap. added angular module and controller

--- gulpfile.js
+++ gulpfile.js
@@ -17,7 +17,7 @@ gulp.task('copyhtml', function() {
 gulp.task('index', function () {
     var target = gulp.src('./index.html');
     // It's not necessary to read the files (will speed up things), we're only after their paths:
-    var sources = gulp.src(['./scripts/**/*.js', './stylesheets/**/*.css'], {read: false});
+    var sources = gulp.src(['./app/**/*.js', './stylesheets/**/*.css'], {read: false});
 
     return target.pipe(inject(sources))
         .pipe(gulp.dest('./'));
