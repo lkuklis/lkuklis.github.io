ec7343e842e5e91a9315ad924348c4269eb36198
Lukasz Kuklis <lkuklis@gmail.com>
2016-03-19 15:38:22 +0100

    Update 2016-03-19T14:38:19.729Z

---
+++ js/app.js
@@ -0,0 +1,15 @@
+(function(){
+    // declaring the module in one file / anonymous function
+    // (only pass a second parameter THIS ONE TIME as a redecleration creates bugs
+    // which are very hard to dedect)
+    angular.module('main', []);
+})();
+(function(){
+    "use strict";
+    angular.module('main')
+        .controller('publicCtrl',  function(){
+
+        })
+
+
+})();
